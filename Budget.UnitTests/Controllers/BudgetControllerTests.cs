267f459da80ef5912494bfc1ecf0fb9bbd2e956b
Joey Chen <jay.hatelove@gmail.com>
2023-12-21 20:00:38 +0800

    clean up

--- Budget.UnitTests/Controllers/BudgetControllerTests.cs
+++ Budget.UnitTests/Controllers/BudgetControllerTests.cs
@@ -1,11 +1,15 @@
-﻿using Budget.Repositories;
+﻿#region
+
+using Budget.Repositories;
 using Budget.Services;
 using FluentAssertions;
 using NSubstitute;
-using NUnit.Framework.Internal;
+
+#endregion
 
 namespace Budget.UnitTests.Controllers;
 
+[TestFixture]
 public class BudgetControllerTests
 {
     private IBudgetRepository _budgetRepository;
@@ -19,64 +23,28 @@ public class BudgetControllerTests
     }
 
     [Test]
-    public void single_month()
-    {
-        _budgetRepository.GetAll().Returns(new List<Models.Budget>
-            {
-                new()
-                {
-                    YearMonth = "202312",
-                    Amount = 3100
-                }
-            }
-        );
-        var start = new DateTime(2023, 12, 1);
-        var end = new DateTime(2023, 12, 31);
-        var actualAmount = _budgetService.Query(start, end);
-        actualAmount.Should().Be(3100);
-    }
-    
-    [Test]
-    public void partial_month()
-    {
-        GivenBudgets(new List<Models.Budget>
-        {
-            new()
-            {
-                YearMonth = "202312",
-                Amount = 3100
-            }
-        });
-        var actualAmount = WhenQuery(
-            new DateTime(2023, 12, 15), 
-            new DateTime(2023, 12, 31));
-        actualAmount.Should().Be(1700);
-    }
-
-    [Test]
     public void cross_three_months()
     {
         GivenBudgets(new List<Models.Budget>
-        {
-            new()
-            {
-                YearMonth = "202310",
-                Amount = 31
-            },
-            new()
-            {
-                YearMonth = "202311",
-                Amount = 3000
-            },
-            new()
-            {
-                YearMonth = "202312",
-                Amount = 31000
-            },
-            
-        });
+                     {
+                         new()
+                         {
+                             YearMonth = "202310",
+                             Amount = 31
+                         },
+                         new()
+                         {
+                             YearMonth = "202311",
+                             Amount = 3000
+                         },
+                         new()
+                         {
+                             YearMonth = "202312",
+                             Amount = 31000
+                         },
+                     });
         var actualAmount = WhenQuery(
-            new DateTime(2023, 10, 10), 
+            new DateTime(2023, 10, 10),
             new DateTime(2023, 12, 12));
         actualAmount.Should().Be(22 + 3000 + 12000);
     }
@@ -85,16 +53,16 @@ public class BudgetControllerTests
     public void illegal_start_end()
     {
         GivenBudgets(new List<Models.Budget>
-        {
-            new()
-            {
-                YearMonth = "202312",
-                Amount = 3100
-            }
-        });
+                     {
+                         new()
+                         {
+                             YearMonth = "202312",
+                             Amount = 3100
+                         }
+                     });
         var actualAmount = WhenQuery(
             new DateTime(2023, 12, 31),
-            new DateTime(2023, 12, 15)); 
+            new DateTime(2023, 12, 15));
         actualAmount.Should().Be(0);
     }
 
@@ -102,32 +70,68 @@ public class BudgetControllerTests
     public void lack_of_month()
     {
         GivenBudgets(new List<Models.Budget>
-        {
-            new()
-            {
-                YearMonth = "202310",
-                Amount = 31
-            },
-            new()
-            {
-                YearMonth = "202312",
-                Amount = 31000
-            },
-        });
+                     {
+                         new()
+                         {
+                             YearMonth = "202310",
+                             Amount = 31
+                         },
+                         new()
+                         {
+                             YearMonth = "202312",
+                             Amount = 31000
+                         },
+                     });
         var actualAmount = WhenQuery(
-            new DateTime(2023, 10, 10), 
+            new DateTime(2023, 10, 10),
             new DateTime(2023, 12, 12));
         actualAmount.Should().Be(22 + 12000);
+    }
+
+    [Test]
+    public void partial_month()
+    {
+        GivenBudgets(new List<Models.Budget>
+                     {
+                         new()
+                         {
+                             YearMonth = "202312",
+                             Amount = 3100
+                         }
+                     });
+        var actualAmount = WhenQuery(
+            new DateTime(2023, 12, 15),
+            new DateTime(2023, 12, 31));
+        actualAmount.Should().Be(1700);
+    }
+
+    [Test]
+    public void single_month()
+    {
+        _budgetRepository.GetAll()
+                         .Returns(new List<Models.Budget>
+                                  {
+                                      new()
+                                      {
+                                          YearMonth = "202312",
+                                          Amount = 3100
+                                      }
+                                  }
+                         );
+        var start = new DateTime(2023, 12, 1);
+        var end = new DateTime(2023, 12, 31);
+        var actualAmount = _budgetService.Query(start, end);
+        actualAmount.Should().Be(3100);
+    }
+
+    private void GivenBudgets(List<Models.Budget> budgets)
+    {
+        _budgetRepository.GetAll().Returns(budgets);
     }
 
     private decimal WhenQuery(DateTime start, DateTime end)
     {
         var actualAmount = _budgetService.Query(start, end);
         return actualAmount;
-    }
-
-    private void GivenBudgets(List<Models.Budget> budgets)
-    {
-        _budgetRepository.GetAll().Returns(budgets);
     }
 }
