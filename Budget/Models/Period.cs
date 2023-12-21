56e9b17c9b5519efe611d54f85f334b1a7164881
Joey Chen <jay.hatelove@gmail.com>
2023-12-21 20:37:57 +0800

    move Period class to file

---
+++ Budget/Models/Period.cs
@@ -0,0 +1,20 @@
+﻿namespace Budget.Models;
+
+public class Period
+{
+    public Period(DateTime start, DateTime end)
+    {
+        Start = start;
+        End = end;
+    }
+
+    public DateTime End { get; private set; }
+    public DateTime Start { get; private set; }
+
+    public int GetOverlappingDays(Period another)
+    {
+        var overlappingEnd = another.End < End ? another.End : End;
+        var overlappingStart = another.Start > Start ? another.Start : Start;
+        return (overlappingEnd - overlappingStart).Days + 1;
+    }
+}
--- Budget/Services/BudgetService.cs
+++ Budget/Services/BudgetService.cs
@@ -1,29 +1,11 @@
 ﻿#region
 
+using Budget.Models;
 using Budget.Repositories;
 
 #endregion
 
 namespace Budget.Services;
-
-public class Period
-{
-    public Period(DateTime start, DateTime end)
-    {
-        Start = start;
-        End = end;
-    }
-
-    public DateTime End { get; private set; }
-    public DateTime Start { get; private set; }
-
-    public int GetOverlappingDays(Period another)
-    {
-        var overlappingEnd = another.End < End ? another.End : End;
-        var overlappingStart = another.Start > Start ? another.Start : Start;
-        return (overlappingEnd - overlappingStart).Days + 1;
-    }
-}
 
 public class BudgetService
 {
