2b968edf04920fcced0d8648c4a2faf0a140f951
Joey Chen <jay.hatelove@gmail.com>
2023-12-21 20:11:33 +0800

    move calculation of overlapping amount when finding budget

--- Budget/Services/BudgetService.cs
+++ Budget/Services/BudgetService.cs
@@ -28,21 +28,19 @@ public class BudgetService
 
     private decimal GetTotalAmount(DateTime startDate, DateTime endDate, List<Models.Budget> budgets)
     {
-        // var period = new Dictionary<DateTime, int>();
         var currentMonth = startDate;
 
         var totalAmount = 0m;
         while (currentMonth <= endDate)
         {
-            var endOfMonth = new DateTime(
-                currentMonth.Year, currentMonth.Month,
-                DateTime.DaysInMonth(currentMonth.Year, currentMonth.Month));
-            var endOfPeriod = (endOfMonth < endDate) ? endOfMonth : endDate;
-            var daysInMonth = (endOfPeriod - currentMonth).Days + 1;
-            // period.Add(currentMonth, daysInMonth);
             var budget = budgets.SingleOrDefault(b => b.YearMonth == currentMonth.ToString("yyyyMM"));
             if (budget != null)
             {
+                var endOfMonth = new DateTime(
+                    currentMonth.Year, currentMonth.Month,
+                    DateTime.DaysInMonth(currentMonth.Year, currentMonth.Month));
+                var endOfPeriod = (endOfMonth < endDate) ? endOfMonth : endDate;
+                var daysInMonth = (endOfPeriod - currentMonth).Days + 1;
                 var singleDayAmount = budget.Amount / DateTime.DaysInMonth(currentMonth.Year, currentMonth.Month);
                 totalAmount += singleDayAmount * daysInMonth;
             }
