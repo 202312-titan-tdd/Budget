979decfd6fb1cb7fb42bd02897d38f8cac2d78c0
Joey Chen <jay.hatelove@gmail.com>
2023-12-21 20:09:51 +0800

    clean up

--- Budget/Services/BudgetService.cs
+++ Budget/Services/BudgetService.cs
@@ -26,23 +26,11 @@ public class BudgetService
         return GetTotalAmount(start, end, budgets);
     }
 
-    // private Dictionary<DateTime, int> GetBudgetPeriod(DateTime startDate, DateTime endDate, List<Models.Budget> budgets)
     private decimal GetTotalAmount(DateTime startDate, DateTime endDate, List<Models.Budget> budgets)
     {
         var period = new Dictionary<DateTime, int>();
         var currentMonth = startDate;
 
-        // return budgets
-        //        .Join(
-        //            budgetPeriod,
-        //            budget => budget.YearMonth,
-        //            period => period.Key.ToString("yyyyMM"),
-        //            (budget, period) =>
-        //            {
-        //                var singleDayAmount = budget.Amount / DateTime.DaysInMonth(period.Key.Year, period.Key.Month);
-        //                return singleDayAmount * period.Value;
-        //            })
-        //        .Sum(monthAmount => monthAmount);
         var totalAmount = 0m;
         while (currentMonth <= endDate)
         {
@@ -64,6 +52,5 @@ public class BudgetService
         }
 
         return totalAmount;
-        // return period;
     }
 }
